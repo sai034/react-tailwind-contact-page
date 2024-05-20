7b49d3f0ed6983350a12c7015b50c50688c94209
Sai Sourabh <saisourabhcherukuru@gmail.com>
2024-05-20 13:06:20 +0530

    tailwind changes

--- src/App.jsx
+++ src/App.jsx
@@ -37,23 +37,25 @@ function App() {
        </div>
        </div>
       </div>
-        <br />
+        {/* <br /> */}
        {/* Setting width,height,left,height and text-align using css*/}
-        <div className='container3'>
+        <div className='mt-8 container3'>
           <span>We're excited to hear about your idea, whether it's straightforward or complex. At NewDefines, we thrive on meeting new challenges 
             and connecting with new faces. If you're unsure where to start, share more about your company and how you think we can help. Team
              NewDefines is here to listen, adjust, and execute changes tailored to your business needs. It's that simple</span>
           </div>
-          <br />   <br />   <br />   <br />   <br />   <br />    
+          {/* <br />   <br />   <br />   <br />   <br />   <br />     */}
         {/* Setting margin-left and margin-right: 64px and setting the content in center using flex and setting margin left and right:64px*/}
-        <div className="mx -16 flex justify-center items-center">
+        
+        <div className="mx-16 flex justify-center items-center ">
           {/* Setting the form space */}
-        <div className='form-space'>
+        <div className='form-place '>
            {/* Setting the form usng ant design */}
            <Forms />
        
       </div>
       </div>
+      
       <br/> <br />
      {/* Setting footer using css (height and top) and tailwiind css (width) */}
       <div className='w-full container2'>
